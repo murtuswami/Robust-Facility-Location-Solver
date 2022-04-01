cf0bdf63a7cb3a60dc391e4b21f256eb7065de45
murtuswami <harsharamachandran14@gmail.com>
2022-04-01 20:57:49 +0100

    Refactor and comment optimization model

---
+++ optimizationModel/processPyomoData.py
@@ -0,0 +1,53 @@
+
+import tkinter
+from tkinter import filedialog
+
+"""
+Takes Data from file and converts into into solveable form for Pyomo model 
+
+Parameters
+    n/a 
+Returns 
+   n: number of facilities
+   m: number of customers 
+   d: dictionary of distances in form (Facilty,Customer): Distance 
+   o: dictionary of opening costs in the form  Facility : Cost
+   arrRepresentation: A 2D representation of d (for calculating deviations )
+   N: Array containing facility identities
+   M Array containing customer identities
+
+   
+Description 
+    Creates an Pyomo model object for solving a given Uncapacitated Facility Location Problem instance 
+"""
+
+def processPyomoData():
+    arr = []
+    tkinter.Tk().withdraw() # prevents an empty tkinter window from appearing
+    print("Please select file with input data")
+    inputdatapath = filedialog.askopenfilename()
+    file = open(inputdatapath)
+    for line in file:
+        arr.append(line.rstrip().split())
+
+    initVals = arr[1]
+    del arr[:2]
+    m = int(initVals[1])
+    n = int(initVals[0]) 
+    print(m)
+    M = [*range(1,m+1)]
+    N = [*range(1,n+1)]
+    o ={}
+    d={}
+    arrRepresentation = [[] for _ in range(0,m)]
+
+    for x in arr:
+        fac = x[0] # first entry is the facility identifier 
+        cost = x[1] # second entry is the cost of opening said facility 
+        o.update({int(fac):int(cost)})
+        del x[:2]
+            
+        for index, c in enumerate(x): # all other entries are cost of connecting to customer number, counting starts from 1 
+            d.update({(int(fac),index+1):int(c)})
+            arrRepresentation[index].append(int(c))
+    return n,m,d,o,arrRepresentation,N,M
--- optimizationModel/solver.py
+++ optimizationModel/solver.py
@@ -6,39 +6,19 @@ import os
 import pyomo.environ as pyo
 from pyomo.opt import SolverFactory
 from pyomo.environ import *
-
+from processPyomoData import processPyomoData
+from model import make_model
 
 ### Get Data from file and convert it into format appropriate for Pyomo Model ### 
-arr = []
-tkinter.Tk().withdraw() # prevents an empty tkinter window from appearing
-print("Please select file with input data")
-inputdatapath = filedialog.askopenfilename()
-print(inputdatapath)
-filename = os.path.basename(inputdatapath)
-file = open(inputdatapath)
-for line in file:
-    arr.append(line.rstrip().split())
 
-initVals = arr[1]
-del arr[:2]
-m = int(initVals[1])
-n = int(initVals[0]) 
-setM = [*range(1,m+1)]
-setN = [*range(1,n+1)]
-paramo ={} #Opening Costs 
-paramd={}  #Distances 
 
-for x in arr:
-    fac = x[0]
-    cost = x[1]
-    paramo.update({int(fac):int(cost)})
-    del x[:2]
-    for index, c in enumerate(x):
-        paramd.update({(int(fac),index+1):int(c)})
+n,m,d,o,arrRepresentation,N,M = processPyomoData()
+
+### Solve Model and write results to file ###
 
 opt = SolverFactory('cplex') 
 opt.options['timelimit'] = 180 #3 mins time limit 
-model = make_model(paramd,paramo,setN,setM)
+model = make_model(d,o,N,M)
 opt.solve(model,tee = True,logfile ="results.txt") #Solve model with verbose settings 
 print( pyo.value(model.obj) )
 
